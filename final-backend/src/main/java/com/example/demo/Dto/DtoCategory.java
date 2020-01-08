ce026ff9f1c972e2d58cc7ec836803c61c4c5be3
cony ross <jardlml1209@gmail.com>
2020-01-08 13:49:06 -0300

    Creo que esta bien jjsjs

---
+++ final-backend/src/main/java/com/example/demo/Dto/DtoCategory.java
@@ -0,0 +1,33 @@
+package com.example.demo.Dto;
+
+import javax.persistence.Column;
+
+public class DtoCategory {
+    private Long idDto;
+    private String nameDto;
+    private String descriptionDto;
+
+    public Long getIdDto() {
+        return idDto;
+    }
+
+    public void setIdDto(Long idDto) {
+        this.idDto = idDto;
+    }
+
+    public String getNameDto() {
+        return nameDto;
+    }
+
+    public void setNameDto(String nameDto) {
+        this.nameDto = nameDto;
+    }
+
+    public String getDescriptionDto() {
+        return descriptionDto;
+    }
+
+    public void setDescriptionDto(String descriptionDto) {
+        this.descriptionDto = descriptionDto;
+    }
+}
