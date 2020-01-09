3cbf243dd192e62e299135514775e0373d932e66
Javier Rojas <jardlml1209@gmail.com>
2020-01-09 12:14:21 -0300

    algo poco

---
+++ final-backend/src/main/java/com/example/demo/Dto/DtoCategory.java
@@ -0,0 +1,31 @@
+package com.example.demo.Dto;
+
+public class DtoCategory {
+    private Long idDto;
+    private String nameDto;
+    private String descriptionDto;
+
+    public Long getIdDto() {
+        return idDto;
+    }
+
+    public void setIdDto(Long idDto) {
+        this.idDto = idDto;
+    }
+
+    public String getNameDto() {
+        return nameDto;
+    }
+
+    public void setNameDto(String nameDto) {
+        this.nameDto = nameDto;
+    }
+
+    public String getDescriptionDto() {
+        return descriptionDto;
+    }
+
+    public void setDescriptionDto(String descriptionDto) {
+        this.descriptionDto = descriptionDto;
+    }
+}
