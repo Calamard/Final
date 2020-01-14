825ff5162f1291d27f25cfb49b9c735ed399b52b
Calamard <guido.huechumilla@outlook.com>
2020-01-14 12:34:38 -0300

    ojala resulte

--- final/src/app/app.module.ts
+++ final/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { UsuarioComponent } from './pages/usuario/usuario.component';
