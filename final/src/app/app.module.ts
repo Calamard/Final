5d1cbb3d088260c93fd5daae72e7ef3a7ece70d5
Calamard <guido.huechumilla@outlook.com>
2020-01-17 05:45:01 -0300

    avance de componentes login y home"

--- final/src/app/app.module.ts
+++ final/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDatepickerModule, MatNativeDateModule } from '@angular/material';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { UsuarioComponent } from './pages/usuario/usuario.component';
