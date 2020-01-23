c659dff03a41888eff501317c9ec19dc8823daf8
joseCris <elsolbrillaenlaluna@gmail.com>
2020-01-22 22:32:52 -0300

    error pero esta con el login

--- final/src/app/Service/login.service.ts
+++ final/src/app/Service/login.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { User } from '../model/User';
+import { Injectable } from '@angular/core';
+import { Login } from '../model/Login';
 
 @Injectable({
   providedIn: 'root'
@@ -18,7 +18,23 @@ export class LoginService {
   };
   constructor(private http:HttpClient) { }
   
-  public login (name:string , lastNameDto:string,){
-      
+  public login (name:string , last:string, rut:string,genre:string,age:number,type:string,email:string,password:string, country:string){
+      let body ={
+        nameDto:name,
+        lastNameDto:last,
+        rutDto:rut,
+        genreDto:genre,
+        ageDto:age,
+        typeDto:type,
+
+        emailDto:email,
+        passwordDto:password,
+
+        contryDto:country,
+        
+         
+      }
+      return this.http.POST("http://localhost:8090/swagger-ui.html#/login-controller/crearUsuaioUsingPOST",body,this.headersOptions)
+  }
 }
-}
+
--- final/src/app/pages/sigin-up/sigin-up.component.ts
+++ final/src/app/pages/sigin-up/sigin-up.component.ts
@@ -2,12 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'src/app/Service/login.service';
 
 @Component({
+
   selector: 'app-sigin-up',
   templateUrl: './sigin-up.component.html',
   styleUrls: ['./sigin-up.component.css']
+  
 })
+
 export class SiginUpComponent implements OnInit {
   name: string;
+  last:string;
+  rut:string;
+  genre:string;
+  age:number;
+  type:string;
+  email:string;
+  password:string;
+  country:string;
   
   constructor(private loginService: LoginService) { }
 
@@ -15,7 +26,7 @@ export class SiginUpComponent implements OnInit {
   }
 
   creatteUser(){
-    this.loginService.login(this.name,).subscribe(
+    this.loginService.login(this.name, this.last, this.rut, this.genre ,this.age, this.type, this.email,this.password, this.country).subscribe(
       data => {
         console.log(data);
       },
